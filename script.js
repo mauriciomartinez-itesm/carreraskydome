23d93bcfd2d56fbb0d9c07f458ee6373a3e599c7
mauriciomartinez-itesm <33909685+mauriciomartinez-itesm@users.noreply.github.com>
2023-08-22 17:45:59 +0000

    Carga de archivos general mediante Git

--- script.js
+++ script.js
@@ -223,10 +223,3 @@ function checkpoint(e) {
 
 
 }
-
-/*
-document.querySelector(".blocker").addEventListener("mouseenter", blockerOver);
-
-function blockerOver(e) {
-  document.querySelector(".a-canvas.a-grab-cursor").css("cursor", "defaut !important;")
-}*/
